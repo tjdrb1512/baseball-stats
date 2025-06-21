fe8b45b9ad1befa43a4a80961dd770923de0d94e
tjdrb1512 <tjdrb1512@gmail.com>
2025-06-21 13:46:48 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BaseballStatsViewer from './components/BaseballStatsViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseballStatsViewer />
+    </div>
+  );
+}
+
+export default App;
